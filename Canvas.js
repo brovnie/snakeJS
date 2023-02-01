13ec643d698123671b2664093e27651ea186df74
brovnie-ubuntu <maarl.deenis@gmail.com>
2023-02-01 14:37:40 +0100

    connected Snake with Canvas module

--- Canvas.js
+++ Canvas.js
@@ -1,18 +1,19 @@
+import {Snake} from "./Snake.js";
+
 class Canvas {
     constructor() {
         this.canvas = document.getElementById("game");
         this.ctx = this.canvas.getContext("2d");
         this.canvasWidth = this.canvas.width;
-        this.canvasHeight = this.canvas.height
+        this.canvasHeight = this.canvas.height;
+        this.tiles = 50;
+        this.snake = new Snake();
     }
     clearCanvas() {
+        this.snake.test();
         this.ctx.clearRect(0, 0, this.canvasWidth,this.canvasHeight);
     }
-    drawCanvas() {
-
-    }
-
 }
 
-let canvas = new Canvas();
+window.canvas = new Canvas();
 canvas.clearCanvas();
--- Snake.js
+++ Snake.js
@@ -1,5 +1,11 @@
-class Snake {
-constructor () {
-    
-}
+export class Snake {
+    constructor () {
+        this.snakeBody = [];
+        this.snakeHeadX = 250;
+        this.snakeHeadY = 250;
+        this.tileLength = 2;     
+    }
+    test() {
+    console.log("function")
+    }
 }
